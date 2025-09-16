2a8c5dd551c4514076915c990156febb2a668a54
ict-204 <sme0215@dlsud.edu.ph>
2025-09-16 09:34:31 +0800

    Feat: complete btnConsole functionality

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-var spanOutput,
-    btnDemo, btnWrite, btnAlert;
+var spanOutput, main,
+    btnDemo, btnWrite, btnAlert, btnConsole;
 
 function getElements() {
     spanOutput = document.querySelector("span.output");
     btnDemo = document.querySelector("button.demo");
     btnWrite = document.querySelector("button.write");
     btnAlert = document.querySelector("button.alert");
+    btnConsole = document.querySelector("button.console");
 }
 
 function sayHello() {
@@ -20,9 +21,14 @@ function alertUser() {
     window.alert("This is an alert box!");
 }
 
+function writeConsole() {
+    console.log("This is a console log message.");
+}
+
 (function() {
     getElements();
     btnDemo.addEventListener("click", sayHello);
     btnWrite.addEventListener("click", writeText);
     btnAlert.addEventListener("click", alertUser);
+    btnConsole.addEventListener("click", writeConsole);
 })();
