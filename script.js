2d912bbf4f6fe9fe5649da5feb73d49d32836fda
ict-204 <sme0215@dlsud.edu.ph>
2025-09-16 09:29:24 +0800

    Feat: complete btnWrite functionality

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 var spanOutput,
-    btnDemo;
+    btnDemo, btnWrite;
 
 function getElements() {
     spanOutput = document.querySelector("span.output");
     btnDemo = document.querySelector("button.demo");
+    btnWrite = document.querySelector("button.write");
 }
 
 function sayHello() {
     spanOutput.textContent = "Hello JavaScript";
 }
 
+function writeText() {
+    document.write("This is written using document.write()");
+}
+
 (function() {
     getElements();
     btnDemo.addEventListener("click", sayHello);
+    btnWrite.addEventListener("click", writeText);
 })();
